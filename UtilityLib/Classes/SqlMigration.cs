3aca7a277a3753ed886e990fd46d9c56cd48ef51
Marc <marc@leftek.com>
2015-03-01 18:49:28 +0800

    Fixed SqlMigration

--- UtilityLib/Classes/SqlMigration.cs
+++ UtilityLib/Classes/SqlMigration.cs
@@ -12,20 +12,21 @@ namespace UtilityLib {
         public void AddMigration(IEnumerable<SqlMigration> Migrations) { Migrations.ForEach(m => _migrations.Add(m)); }
         public IEnumerable<string> GetUpdgradeSql(System.Version DbVersion) { // Note: Don't check DbVersion here since it reflects the Engine.Assembly.Version, so it may be greater than Max.AppVersion.
             if (DbVersion == null) return _migrations.Select(m => m.MigrateSql);
-            if (DbVersion >  _migrations.Max.AppVersion) throw new Exception("Error: Cannot upgrade Database version (" + DbVersion + ") to a lower version (" + _migrations.Max.AppVersion + ")");
+            if (DbVersion >  _migrations.Max.DbVersion) throw new Exception("Error: Cannot upgrade Database version (" + DbVersion + ") to a lower version (" + _migrations.Max.DbVersion + ")");
             //if (DbVersion == _migrations.Max.AppVersion) throw new Exception("Error: Database version (" + DbVersion + ") is already the same as the upgrade (" + _migrations.Max.AppVersion + ")");
-            return _migrations.Where(m => m.AppVersion.CompareTo(DbVersion) > 0).Select(m => m.MigrateSql);
+            return _migrations.Where(m => m.DbVersion.CompareTo(DbVersion) > 0).Select(m => m.MigrateSql);
         }
+        public System.Version MaxDbVersion { get { return _migrations.Last().DbVersion; } }
     }
-    public class SqlMigration {
-        public System.Version AppVersion {get; set;}
+    public class SqlMigration : IComparable<SqlMigration> {
+        public System.Version DbVersion {get; set;}
         public string MigrateSql {get; set;}
-        public SqlMigration(string SqlAppVersion, string SQL) {
-            this.AppVersion = new Version(SqlAppVersion);
+        public SqlMigration(string DbVersion, string SQL) {
+            this.DbVersion = new Version(DbVersion);
             this.MigrateSql = SQL;
         }
-        public override string ToString() { return AppVersion.ToString(); }
-        public override int GetHashCode() { return AppVersion.GetHashCode(); }
-        public override bool Equals(object obj) { return (obj as SqlMigration) != null && (obj as SqlMigration).AppVersion.Equals(AppVersion); }
+        public int CompareTo(SqlMigration other) {
+            return this.DbVersion == other.DbVersion ? 0 : this.DbVersion < other.DbVersion ? -1 : 1;
+        }
     }
 }
