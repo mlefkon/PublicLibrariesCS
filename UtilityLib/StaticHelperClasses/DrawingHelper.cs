56b02c4df3a8714ae345549b734a000037a9f2bd
Marc <marc@leftek.com>
2015-04-01 09:17:57 +0800

    Rename/update to Icon-Drawing helper

--- UtilityLib/StaticHelperClasses/DrawingHelper.cs
+++ UtilityLib/StaticHelperClasses/DrawingHelper.cs
@@ -3,13 +3,20 @@
 // Licensed under MIT License (MIT) (https://github.com/mlefkon/PublicLibrariesCS/blob/master/UtilityLib/license.txt).
 
 namespace UtilityLib {
-    public static class IconHelper {
+    public static class DrawingHelper {
         public static System.Drawing.Icon GetEmbeddedResourceIcon(string IconName) { 
             // Note: IconName format: NameSpace/OptionalRootFolder/OptionalSubFolder/FileName.XXX
             System.Reflection.Assembly a = System.Reflection.Assembly.GetCallingAssembly();
             System.IO.Stream st = a.GetManifestResourceStream(IconName);
             System.Drawing.Icon icn = new System.Drawing.Icon(st);
             return(icn);
+        }
+        public static System.Drawing.Bitmap GetEmbeddedResourceBitmap(string BitmapName) { 
+            // Note: IconName format: NameSpace/OptionalRootFolder/OptionalSubFolder/FileName.XXX
+            System.Reflection.Assembly a = System.Reflection.Assembly.GetCallingAssembly();
+            System.IO.Stream st = a.GetManifestResourceStream(BitmapName);
+            System.Drawing.Bitmap img = new System.Drawing.Bitmap(st);
+            return(img);
         }
     }
 }
